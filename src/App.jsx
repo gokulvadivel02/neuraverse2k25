e4001b8e787c382c6fb0f992cb5717da2896b03b
gokulvadivel02 <gokulvadivel002@gmail.com>
2025-05-08 11:56:00 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
-import React, { useRef } from 'react'
-import Navbar from './sympo project/Navbar'
-import Home from './sympo project/Home'
-import Events from './sympo project/Events'
-import Register from './sympo project/Register'
-import Contact from './sympo project/Contact'
+import React from 'react'
+import Todo from './components/Todo.jsx'
 
 const App = () => {
-  const mainhomeref = useRef(null)
-  const maineventref = useRef(null)
-  const mainscheduleref = useRef(null)
-  const mainregisterref = useRef(null)
-  const maincontactrref = useRef(null)
-  
   return (
-    <div className='bg-black text-white h-fit w-fit ' 
-    style={{background : 'url("/images/bggif001.gif")'}}   >
-      <h1 className='text-black'>hellow</h1>
-      <Navbar home={mainhomeref} event={maineventref} schedule={mainscheduleref} 
-      register={mainregisterref} contact={maincontactrref} /> <br /> 
-      <Home homeref={mainhomeref}/>
-      <Events eventref={maineventref}/>
-      <Register registerref={mainregisterref}/>
-      <Contact contactref= {maincontactrref}/>
+    <div>
+      <Todo/>
     </div>
   )
 }
